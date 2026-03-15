2c4241203c2f9aa24e99147e096e5b5fdcc07aaf
patrick <sbes30208@gmail.com>
2026-03-15 18:44:10 +0800

    Initial Commit

---
+++ Mario/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mario
+//
+//  Created by 114-2Student03 on 2026/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mario/MarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioApp.swift
+//  Mario
+//
+//  Created by 114-2Student03 on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct MarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
